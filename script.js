992b2b79f9148e897541eb387834127c62cadefe
Carlos Mertens <mertenscarlos@yahoo.com>
2021-03-30 23:16:09 +0200

    Create event listener for check button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Event Listener for the Check button
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 Must give a number!';
+  }
+});
