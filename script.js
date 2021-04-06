fd407128f5ba7b7942d909cb9919d5771b54dd75
Carlos Mertens <mertenscarlos@yahoo.com>
2021-04-06 23:15:23 +0200

    Implement Again! button to reset game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Generate secret number
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 // Create score variable
 let score = 20;
@@ -32,4 +32,16 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.score').textContent = 0;
     document.querySelector('body').style.backgroundColor = '#FF0000';
   }
+});
+
+// Event Listener for the play Again! button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
 });
